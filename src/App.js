20f44603ff8d629a76a100e60ed0f224a5e932da
Iván López <ilopez@etarmadillo.com>
2022-03-26 23:00:08 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Responsive, WidthProvider } from "react-grid-layout";
+import _ from "lodash";
+import "/node_modules/react-grid-layout/css/styles.css"
+import "/node_modules/react-resizable/css/styles.css"
+const ResponsiveGridLayout = WidthProvider(Responsive);
 
-function App() {
+function MyFirstGrid() {
+  const [items, setItems] = useState(["a", "b", "c"]);
+
+  const createElement = (x, i) => {
+    return (<div key={x} data-grid={{ x: i, y: 0, w: 1, h: 2 }}>{i}</div>)
+  }
+
+  const onAddItem = () => {
+    setItems(items.concat("d"))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => onAddItem()}>Add</button>
+      <ResponsiveGridLayout
+        className="layout"
+        breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+        cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+      >
+        {_.map(items, createElement)}
+        {/* <div key="a" data-grid={{ x: 0, y: 0, w: 1, h: 2, static: true }}>
+          a
+        </div>
+        <div key="b" data-grid={{ x: 0, y: 0, w: 3, h: 2, minW: 2, maxW: 4 }}>
+          b
+        </div>
+        <div key="c" data-grid={{ x: 0, y: 0, w: 1, h: 2 }}>
+          c
+        </div> */}
+
+      </ResponsiveGridLayout>
     </div>
   );
 }
 
-export default App;
+export default MyFirstGrid;
